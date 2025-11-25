d26b1150548e5e23f71171e60827c1b9c787af75
sanchezjorge001 <sanchezmarinjorge001.alu@stemgranada.com>
2025-11-25 10:44:32 +0100

    .

---
+++ defensorPoloNorte/src/context/CañonContext.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const initialState = {
+    damageDealt: 0,
+    wavegGoal: 100,
+    caramels: 20,
+
+function CañonContext() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CañonContext
