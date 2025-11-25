aa45b8a4470cd716262aa91b6d9acc97543fed04
sanchezjorge001 <sanchezmarinjorge001.alu@stemgranada.com>
2025-11-25 11:39:46 +0100

    .

--- defensorPoloNorte/src/App.jsx
+++ defensorPoloNorte/src/App.jsx
@@ -3,17 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import canion from './assets/torre.png'
-
-import { CañonProvider } from './context/CañonContext'
+import { CañonProvider, useCañon } from './context/CañonContext'
 import UpgardesBoard from './components/UpgardesBoard.jsx'
 
-function App() {
-  
+
+
+function AppContent(){
+  const {state, dispatch} = useCañon();
 
   return (
     <div>
       <div>
         <h1>Defensor del Polo Norte</h1>
+        <button>DISPARAR</button><br/>
+        
         <img src={canion} alt="Cañón" width={200} />
       </div>
       <div>
@@ -24,6 +27,13 @@ function App() {
     </div>
     
   )
+}
+
+
+}
+
+function App() {
+
 }
 
 export default App
